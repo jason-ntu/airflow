dc7d46484d28834b3f703635f73d0c00ef492d45
jason <df41022@gmail.com>
2022-10-14 18:16:24 +0800

    save_to_mysql_stage

--- dags/download_stock_price.py
+++ dags/download_stock_price.py
@@ -22,13 +22,14 @@ Documentation that goes along with the Airflow tutorial located
 """
 from __future__ import annotations
 import os
+import yfinance as yf
+import mysql.connector
 
 # [START tutorial]
 # [START import_module]
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
 from textwrap import dedent
-import yfinance as yf
 from airflow.models import Variable
 
 # The DAG object; we'll need this to instantiate a DAG
@@ -39,47 +40,72 @@ from airflow.operators.python import PythonOperator
 
 # [END import_module]
 
-# [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args={
-    'owner': 'jason',
-    'depends_on_past': False,
-    'email': ['df41022@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
-# [END default_args]
-
-os.environ["no_proxy"]="*"
-
-
-def download_price(**context):
+def get_tickers(context):
     stock_list = Variable.get("stock_list_json", deserialize_json=True)
 
-    stocks = context["dag_run"].conf.get("stocks")
-    print(stocks)
+    stocks = context["dag_run"].conf.get("stocks", None) if ("dag_run" in context and context["dag_run"] is not None ) else None
+
     if stocks:
         stock_list = stocks
+    return stock_list
+
+def download_price(*args, **context):
+    stock_list = get_tickers(context)
 
     for ticker in stock_list:
-        msft = yf.Ticker(ticker)
-        hist = msft.history(period="max")
-
-        with open(f'/home/aurora/airflow/logs/{ticker}.csv', 'w') as writer:
+        dat = yf.Ticker(ticker)
+        hist = dat.history(period="1mo")
+        print(hist.shape[0])
+        with open(get_file_path(ticker), 'w') as writer:
             hist.to_csv(writer, index=True)
-        print("Finished downloading price data for "+ticker)
-   
+        print(f"Downloaded {ticker}")
+
+def get_file_path(ticker):
+    # NOT SAVE in distributed system.
+    return f'/home/aurora/airflow/logs/{ticker}.csv'
+
+def load_price_data(ticker):
+    with open(get_file_path(ticker) , 'r') as reader:
+        lines = reader.readlines()
+        return [[ticker]+line.split(',')[:5] for line in lines if line[:4]!='Date']
+
+
+def save_to_mysql_stage(*args, **context):
+    tickers = get_tickers(context)
+
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='mysql',
+        database='demodb',
+        port=3306
+    )
+
+    mycursor = mydb.cursor()
+    for ticker in tickers: 
+        val = load_price_data(ticker)
+        print(f"{ticker} length={len(val)}   {val[1]}")
+
+        sql = """INSERT INTO stock_prices_stage
+            (ticker, as_of_date, open_price, high_price, low_price, close_price) 
+            VALUES (%s, %s, %s, %s, %s, %s)"""
+        mycursor.executemany(sql, val)
+
+        mydb.commit()
+
+        print(mycursor.rowcount, "record inserted.")
+
+default_args = {
+    'owner' : 'jason'
+}
+
 # [START instantiate_dag]
 with DAG(
     dag_id='Download_Stock_Price',
     default_args=default_args,
-    description='A simple tutorial DAG',
-    schedule=timedelta(days=1),
-    start_date=datetime(2021, 1, 1),
-    catchup=False,
+    description=f'This DAG downloads stock prices and save them into text files.',
+    schedule_interval=None,
+    start_date=days_ago(2),
     tags=['jasondata'],
 ) as dag:
     # [END instantiate_dag]
@@ -92,6 +118,11 @@ with DAG(
         task_id = "download_prices",
         python_callable = download_price,
         provide_context=True
+    )
+
+    save_to_mysql_task = PythonOperator(
+        task_id = 'save_to_database',
+        python_callable = save_to_mysql_stage,
     )
 
 # [END tutorial]
